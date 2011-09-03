ac977b2e72305a0e0bf092652d6921b816f8d44d
Jarrell Waggoner <malloc47@gmail.com>
2011-09-03 17:45:41 -0400

    more utility defines

--- common.h
+++ common.h
@@ -26,10 +26,17 @@ typedef struct {
 #define TYPE_FILE 3;
 
 #define FOR_l(i,v) for(unsigned int (i)=0;(i)<(v).size();(i)++)
-#define FLATTEN_l(l,s) \
+#define FORB_l(i,v) for(unsigned int (i)=(v).size()-1;(i)>0;(i)++)
+#define FLATTEN_l(l,s)							\
   for(unsigned int flatten_var=0; flatten_var<(l).size(); flatten_var++) \
     s=s + (flatten_var ? " " : "") + (l)[flatten_var];
 #define CONTAINS_l(l,s)					\
   (std::find((l).begin(), (l).end(),(s)) != (l).end())
+
+#define CONCAT2_l(p,q,v)			\
+  list_t (v);					\
+  (v).reserve((p).size()+(q).size());		\
+  (v).insert((v).end(),(p).begin(),(p).end());	\
+  (v).insert((v).end(),(q).begin(),(q).end());
 
 #endif
