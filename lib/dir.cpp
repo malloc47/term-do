bc2540018937af5d1a433e6be897f11a647e32a3
Jarrell Waggoner <malloc47@gmail.com>
2011-09-03 23:23:04 -0400

    works satisfactorily with one outstanding bug: no way to enter a directory without a file, currently

--- lib/dir.cpp
+++ lib/dir.cpp
@@ -16,9 +16,14 @@ list_t tokens;
 const string pwd = string(getenv("PWD"))+"/";
 const string home = string(getenv("HOME"))+"/";
 
-bool valid(string dir){
+bool valid_dir(string dir){
   struct stat entry;
   return !(stat(dir.c_str(), &entry) < 0) && S_ISDIR(entry.st_mode);
+}
+
+bool valid_file(string file){
+  struct stat entry;
+  return !(stat(file.c_str(), &entry) < 0) && S_ISREG(entry.st_mode);
 }
 
 bool isdir(string token) {return token[token.size()-1]=='/';}
@@ -38,6 +43,8 @@ list_t ls_d(string path) {
       output.push_back(string(file->d_name)+"/");
   }
   closedir(dirp);
+  if(path.compare("/"))
+     output.push_back("../");
   return output;
 }
 
@@ -62,7 +69,7 @@ list_t base_list() {
   list_t dir = ls_d(pwd);
   list_t files = ls_f(pwd);
   CONCAT2_l(dir,files,output);
-  output.push_back("../");
+  output.push_back("/");
   output.push_back("~/");
   return output;
 }
@@ -77,42 +84,43 @@ list_t list() {
   if(tokens.empty()) {
     return base_list();
   }
-  else {
+  else { // this case may not be obvious, so here goes...
+    // look at the last token to see if we're completing a directory
+    // (if not, just show pwd)
     if(!isdir(tokens[tokens.size()-1])) return base_list();
     string listing="";
     
-    // unsigned int start = tokens.size()-1;
-    // for(;start>0;start++)
-    //   if(tokens[start].at(tokens[start].size()-1)=='/')
-    // 	break;
-
-    // for(unsigned int i=start;i<tokens.size();i++)
-    //   output = output + tokens[i];
-
-    // find index of first directory listing
+    // * walk *backwards* through the list to find the "first"
+    //   directory entry
+    // * this is to handle cases such as
+    //     {cmd} first/directory/path/ --switch second/directory/path/
+    //   where we only want to find second/ as the starting directory
+    //   token so we don't end up trying to get the directory listing
+    //   for /first/directory/path/second/directory/path
+    // * this is done here by finding the first non-directory token
+    //   and setting "start" to be its index
     unsigned int start = tokens.size()-1;
     for(;start>0;start--)
       if(!isdir(tokens[start-1])) {
     	break;
       }
 
+    // now walk from the starting directory token (e.g. second/) to
+    // the latest token, concatenating the directories
     for(unsigned int i=start;i<tokens.size();i++)
       listing = listing + (tokens[i].compare("~/") ? tokens[i] : home);
 
-    // FOR_l(i,tokens)
-    //   if(tokens[i].at(tokens[i].size()-1)=='/')
-    // 	pwd = pwd + tokens[i];
-
+    // get both a directory and file listing
     list_t dir = ls_d(listing);
     list_t files = ls_f(listing);
+    // concatenate the two listings
     CONCAT2_l(dir,files,output);
-    output.push_back("../");
-    output.push_back("~/");
     return output;
   }
   return list_t();
 }
 
+// helper plugin--no need to return a complete command
 string cmd() {
   return "";
 }
