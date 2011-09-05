b63240d1900852eb9ce365ce8e3f5b2126d35c89
Jarrell Waggoner <malloc47@gmail.com>
2011-09-05 17:16:25 -0400

    now only handles commands in its dictionary (no more collissions with 		other plugins)

--- lib/launcher.cpp
+++ lib/launcher.cpp
@@ -25,11 +25,17 @@ list_t splitString(const string s, char delim) {
   return elems;
 }
 
+bool is_cmd(string input) {
+  return find(dictionary.begin(), dictionary.end(), input) != dictionary.end();
+}
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void init() {}
+void init() {
+
+}
 
 void update(list_t new_tokens) {tokens = new_tokens;}
 
@@ -59,61 +65,70 @@ list_t list() {
     return dictionary;
   }
   else {
-    if(!tokens.at(0).compare("vim") && tokens[tokens.size()-1][tokens[tokens.size()-1].size()-1] != '/') {
-      list_t params;
-      params.push_back("--");
-      params.push_back("-v");
-      params.push_back("-e");
-      params.push_back("-s");
-      params.push_back("-d");
-      params.push_back("-y");
-      params.push_back("-R");
-      params.push_back("-Z");
-      params.push_back("-m");
-      params.push_back("-M");
-      params.push_back("-b");
-      params.push_back("-l");
-      params.push_back("-C");
-      params.push_back("-N");
-      params.push_back("-V");
-      params.push_back("-D");
-      params.push_back("-n");
-      params.push_back("-r");
-      params.push_back("-r");
-      params.push_back("-L");
-      params.push_back("-A");
-      params.push_back("-H");
-      params.push_back("-F");
-      params.push_back("-T");
-      params.push_back("-u");
-      params.push_back("--noplugin");
-      params.push_back("-p");
-      params.push_back("-o");
-      params.push_back("-O");
-      params.push_back("+");
-      params.push_back("--cmd");
-      params.push_back("-c");
-      params.push_back("-S");
-      params.push_back("-s");
-      params.push_back("-w");
-      params.push_back("-W");
-      params.push_back("-x");
-      params.push_back("--startuptime");
-      params.push_back("-i");
-      params.push_back("-h");
-      params.push_back("--help");
-      params.push_back("--version");
-      return params;
-    }
+    // if(!tokens.at(0).compare("vim") && tokens[tokens.size()-1][tokens[tokens.size()-1].size()-1] != '/') {
+    //   list_t params;
+    //   params.push_back("--");
+    //   params.push_back("-v");
+    //   params.push_back("-e");
+    //   params.push_back("-s");
+    //   params.push_back("-d");
+    //   params.push_back("-y");
+    //   params.push_back("-R");
+    //   params.push_back("-Z");
+    //   params.push_back("-m");
+    //   params.push_back("-M");
+    //   params.push_back("-b");
+    //   params.push_back("-l");
+    //   params.push_back("-C");
+    //   params.push_back("-N");
+    //   params.push_back("-V");
+    //   params.push_back("-D");
+    //   params.push_back("-n");
+    //   params.push_back("-r");
+    //   params.push_back("-r");
+    //   params.push_back("-L");
+    //   params.push_back("-A");
+    //   params.push_back("-H");
+    //   params.push_back("-F");
+    //   params.push_back("-T");
+    //   params.push_back("-u");
+    //   params.push_back("--noplugin");
+    //   params.push_back("-p");
+    //   params.push_back("-o");
+    //   params.push_back("-O");
+    //   params.push_back("+");
+    //   params.push_back("--cmd");
+    //   params.push_back("-c");
+    //   params.push_back("-S");
+    //   params.push_back("-s");
+    //   params.push_back("-w");
+    //   params.push_back("-W");
+    //   params.push_back("-x");
+    //   params.push_back("--startuptime");
+    //   params.push_back("-i");
+    //   params.push_back("-h");
+    //   params.push_back("--help");
+    //   params.push_back("--version");
+    //   return params;
+    // }
     return list_t();
   }
 }
 
 string cmd() {
-  string output = "";
-  FOR_l(i,tokens)
-    output = output + (i? (tokens[i-1][tokens[i-1].size()-1]=='/' ? "" : " ") :"") + tokens[i];
-  return output;
+  // recognize only exec-able commands 
+  if((!tokens.empty() && is_cmd(tokens[0])) ) { // || 
+     // (tokens.size() > 1 && (!tokens[0].compare("time") ||
+     // 			    !tokens[0]lcompare("watch")) &&
+     //  is_cmd(tokens[1]))) {
+    string output = "";
+    // glob together tokens
+    FOR_l(i,tokens)
+      // output = output + (i? (tokens[i-1][tokens[i-1].size()-1]=='/' ? "" : " ") :"") + tokens[i];
+      output = output + (i? (is_dir(tokens[i-1]) ? "" : " ") :"") + tokens[i];
+    return output;
+  }
+  return "";
 }
 
 #ifdef __cplusplus
