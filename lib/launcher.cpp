98a0bdfead86105037be4cc24eb7ea3c7fc6148c
Jarrell Waggoner <malloc47@gmail.com>
2011-09-19 19:16:00 -0400

    removed cruft regarding old format

--- lib/launcher.cpp
+++ lib/launcher.cpp
@@ -9,15 +9,13 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <functional>
 #include "plugin.h"
 #include "../tst.h"
 #include "../searcher.h"
-// #include <boost/algorithm/string.hpp>
-#include <functional>
 #include "../completion.hpp"
 
 using namespace std;
-// using namespace boost;
 
 typedef struct {
   string completion;
@@ -29,46 +27,7 @@ public:
   Launcher() {
     dictionary = new TST();
     getExec(dictionary);
-    // readConfig("launcher");
-    readConfig2("launcher2");
-    // Completion test("git");
-    // list_t test_list,test_list2,test_list3,test_list4,test_search;
-    // test_list4.push_back("commit");
-    // test_list.push_back("add");
-    // test_search=test_list;
-    // test_list2=test_list;
-    // test_list.push_back("-f");
-    // test_list2.push_back("-a");
-    // test_list3=test_list2;
-    // test_list3.push_back("hi");
-    // printf("\r\nInserting 1\n");
-    // test.insert(test_list);
-    // printf("\r\nInserting 2\n");
-    // test.insert(test_list2);
-    // test.insert(test_list3);
-    // test.insert(test_list4);
-    // printf("\r\nSearching\n");
-    // list_t output = test.search(test_search);
-    // printf("\r\n-------------------------\n");
-    // FOR_l(i,output) {
-    //   printf("\r\n%s\n",output[i].c_str());
-    // }
-
-    // list_t test_search2;
-    // list_t output2 = test.search(test_list2);
-    // printf("\r\n-------------------------\n");
-    // FOR_l(i,output2) {
-    //   printf("\r\n%s\n",output2[i].c_str());
-    // }
-
-    // list_t test_search3;
-    // test_search3.push_back("none");
-    // test_search3.push_back("-f");
-    // list_t output3 = test.search(test_search3);
-    // printf("\r\n-------------------------\n");
-    // FOR_l(i,output3) {
-    //   printf("\r\n%s\n",output3[i].c_str());
-    // }
+    readConfig2("launcher");
   }
 
   ~Launcher() {delete dictionary;}
@@ -85,12 +44,6 @@ public:
   }
 
   list_t list() {
-    // return +x files on $PATH if no command has been typed yet, or if
-    // its a command that takes other commands (e.g. watch)
-
-    // if(tokens.empty() || (!tokens[0].compare("time") || 
-    // 			  !tokens[0].compare("watch")))
-
     if(tokens.empty())
       return dictionary->sort();
 
@@ -111,11 +64,6 @@ public:
     input.push_back(tokens.back());
     // Pull top-level completion, if it works
     list_t output = completions_new[command]->search(input);
-    // printf("\r\nNEW--------------------\n");
-    // FOR_l(i,input) printf("\r%s\n",input[i].c_str());
-    // printf("\rNEW--------------------\n");
-    // FOR_l(i,output) printf("\r%s\n",output[i].c_str());
-    // printf("\r\nNEW--------------------\n");
 
     // Walk backwards through the tokens, adding each and re-searching
     // TODO: make this non-greedy
@@ -123,11 +71,6 @@ public:
       if(output.empty()) {
 	input.insert(input.begin(),tokens[pos]);
 	output = completions_new[command]->search(input);
-	// printf("\r\nNEW--------------------\n");
-	// FOR_l(i,input) printf("\r%s\n",input[i].c_str());
-	// printf("\rNEW--------------------\n");
-	// FOR_l(i,output) printf("\r%s\n",output[i].c_str());
-	// printf("\r\nNEW--------------------\n");
       }
     }
 
@@ -136,12 +79,14 @@ public:
       output = completions_new[command]->all();
 
     FOR_l(i,output) {
+      // Add in function completions
       if(functions.count(output[i])) {
 	output.erase(output.begin()+i);
 	list_t new_output = execCmd(functions[output[i]]);
 	FOR_l(j,new_output) output.push_back(new_output[j]);
 	break;
       }
+      // and alias completions
       else if(aliases.count(output[i])) {
 	output.erase(output.begin()+i);
 	list_t new_output = splitString(aliases[output[i]],' ');
@@ -151,32 +96,6 @@ public:
     }
 
     return output;
-
-    // completion_t comp = completions[command];
-
-    // list_t output, comp_l = splitString(comp.completion,' ');
-    
-    // FOR_l(i,comp_l) {
-    //   if(!comp_l[i].empty() && comp_l[i][0] == '_') {
-    // 	list_t new_output = execCmd(functions[comp_l[i]]);
-    //     FOR_l(j,new_output) output.push_back(new_output[j]);
-    //   }
-    //   else
-    // 	output.push_back(comp_l[i]);
-    // }
-
-    // if(!tokens.back().compare(command) || !comp.subcompletions.count(tokens.back()))
-    //   return output;
-
-    // string subcomp = comp.subcompletions[tokens.back()];
-    // list_t new_output = splitString(subcomp,' ');
-    // // TODO: call functions here too
-    // FOR_l(i,new_output) output.push_back(new_output[i]);
-
-    // return output;
-
-    // else
-    //   return list_t();
   }
 
   // string complete() {return tokens.empty() ? "" : "dir";}
@@ -233,11 +152,7 @@ private:
 
   bool is_cmd(string input) {
     return dictionary->search(input);
-    // return find(dictionary.begin(), dictionary.end(), input) != dictionary.end();
   }
-  // string trim(string s) {
-  //   return s.erase(remove_if(s.begin(), s.end(), isspace), s.end());
-  // }
 
   map<string,completion_t> completions;
   map<string,string> functions;
@@ -249,19 +164,16 @@ private:
     while(!file.eof()) {
       string head, tail;
       getline(file,head,':');
-      // http://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
       head=trim(head);
       getline(file,tail);
       tail=trim(tail);
       
       // function entry
       if(!head.empty() && head[0] == '_') {
-	// cout << "\r\n" << "function -> " << head << " : " << tail << "\n";
 	functions.insert(pair<string,string>(head,tail));
       }
       // first completion entry
       else if(!completions.count(head)) {
-	// cout << "\r\n" << "completions -> " << head << " : " << tail << "\n";
 	completion_t completion;
 	completion.completion=tail;
 	completions.insert(pair<string,completion_t>(head,completion));
@@ -273,7 +185,6 @@ private:
 	string rest = assembleLine(line,1);
 	first=trim(first);
 	rest=trim(rest);
-	// cout << "\r\n" << "switches -> " << first << " : " << rest << "\n";
 	completions[head].subcompletions.insert(pair<string,string>(first,rest));
       }
     }
@@ -285,7 +196,6 @@ private:
     string cmd;
     list_t history;
     int prev_size = 0;
-    // printf("\r\nStart\n");
     while(!file.eof()) {
       string head,tail;
       getline(file,head,':');
@@ -296,7 +206,6 @@ private:
       }
 
       // New command
-      // printf("\r\nLine\n");
       if(head.empty()) continue;
       if(head[0] == '_')
 	functions.insert(pair<string,string>(trim(head),trim(tail)));
@@ -307,12 +216,10 @@ private:
 	aliases.insert(pair<string,string>(trim(head),trim(tail)));
       else if(head[0] != '|') {
 	cmd=trim(head);
-	// printf("\r\nSetting cmd to %s\n",cmd.c_str());
 	completions_new.insert(pair< string,Completion* >(cmd,new Completion(cmd)));
 	list_t line = splitString(tail,' ');
 	FOR_l(i,line) {
 	  if(line.empty()) continue;
-	  // printf("\r\n%s\n",line[i].c_str());
 	  completions_new[cmd]->insert(trim(line[i]));
 	}
 	prev_size=0;	
@@ -324,14 +231,11 @@ private:
 	string new_head;
 	// Determine new stack level
 	for(unsigned int i=0; i<head.size(); i++) {
-	  // printf("i==%d c==%c\n",i,head[i]);
 	  if(head[i] == '|') new_size++;
 	  else break;
 	}
 	// Remove stack chars from head
 	new_head = trim(head.substr(new_size));
-	// printf("\r\nInserting %s\n",new_head.c_str());
-	// printf("\r\nPrev:%d New: %d\n",prev_size,new_size);
 	// Stack should only grow by one
 	if(new_size > prev_size && new_size-prev_size == 1) {
 	  history.push_back(new_head);
@@ -353,9 +257,6 @@ private:
 	}
 	// History now in order, so push command
 	if(!new_head.empty()) {
-	  // printf("\r\nOLD--------------------\n");
-	  // FOR_l(i,history) printf("\r\n%s\n",history[i].c_str());
-	  // printf("\r\nOLD--------------------\n");
 	  completions_new[cmd]->insert(history);
 	}
 	// If there are any trailing commands, add them
@@ -365,9 +266,6 @@ private:
 	    if(line.empty()) continue;
 	    list_t new_entry = history;
 	    new_entry.push_back(trim(line[i]));
-	    // printf("\r\nOLD--------------------\n");
-	    // FOR_l(i,new_entry) printf("\r\n%s\n",new_entry[i].c_str());
-	    // printf("\r\nOLD--------------------\n");
 	    completions_new[cmd]->insert(new_entry);
 	  }
 	}
@@ -375,13 +273,6 @@ private:
       }
     }
     file.close();
-    // list_t search;
-    // search.push_back("--help");
-    // // search.push_back("-f");
-    // printf("\r\nB--------------------\n");
-    // list_t output = completions_new["mplayer"]->search(search);
-    // FOR_l(i,output) printf("\r\n%s\n",output[i].c_str());
-    // printf("\r\nA--------------------\n");
   }
   list_t execCmd(string cmd) {
     FILE *fpipe;
